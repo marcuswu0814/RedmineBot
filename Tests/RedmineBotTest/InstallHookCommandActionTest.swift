2af58a8173546fe5994b7d7af1d39ffe25197a23
Marcus Wu <marcus.wu0814@gmail.com>
2018-04-05 17:53:41 +0800

    Fix InstallHookCommandAction bug
    And add test to cover all cases of installHookCommandAction

--- Tests/RedmineBotTest/InstallHookCommandActionTest.swift
+++ Tests/RedmineBotTest/InstallHookCommandActionTest.swift
@@ -4,7 +4,7 @@ import PathKit
 @testable import RedmineBotCore
 
 class InstallHookCommandActionTest: XCTestCase {
-    
+
     override class func setUp() {
         TempGitRepo.setUP()
     }
@@ -21,6 +21,68 @@ class InstallHookCommandActionTest: XCTestCase {
         
         XCTAssertTrue(sut.postCommitHookPath.exists)
         XCTAssertTrue(sut.postRewriteHookPath.exists)
+        
+        let postRewriteInstallSuccess = try? sut.postRewriteHookPath.read().contains(sut.postRewriteHook)
+        let postCommitHookInstallSuccess = try? sut.postCommitHookPath.read().contains(sut.postCommitHook)
+        
+        XCTAssertTrue(postRewriteInstallSuccess!)
+        XCTAssertTrue(postCommitHookInstallSuccess!)
+        
+        addTeardownBlock {
+            try? sut.postCommitHookPath.delete()
+            try? sut.postRewriteHookPath.delete()
+        }
+    }
+    
+    func test__installHookWhenHookAlreadyInstall__shouldShowWarning() {
+        let mockSystem = MockSystem()
+        let sut = InstallHookCommandAction()
+        sut.system = mockSystem
+        
+        sut.doAction()
+        sut.doAction()
+        
+        let postRewriteAlreadyInstallSuccess = try? sut.postRewriteHookPath.read().contains(sut.postRewriteHook)
+        let postCommitHookAlreadyInstallSuccess = try? sut.postCommitHookPath.read().contains(sut.postCommitHook)
+        
+        XCTAssertTrue(postRewriteAlreadyInstallSuccess!)
+        XCTAssertTrue(postCommitHookAlreadyInstallSuccess!)
+        
+        XCTAssertTrue(mockSystem.printWarningBeInvoke)
+
+        addTeardownBlock {
+            try? sut.postCommitHookPath.delete()
+            try? sut.postRewriteHookPath.delete()
+        }
+    }
+    
+    func test__installHookWhenHookAlreadyExist__shouldAppendToHook() {
+        let mockSystem = MockSystem()
+        let sut = InstallHookCommandAction()
+        sut.system = mockSystem
+        sut.doAction()
+        
+        try? sut.postRewriteHookPath.write("Replace already exist hook")
+        try? sut.postCommitHookPath.write("Replace already exist hook")
+        
+        let postRewriteHookReplaceSuccess = try? sut.postRewriteHookPath.read().contains("Replace already exist hook")
+        let postCommitHookReplaceSuccess = try? sut.postCommitHookPath.read().contains("Replace already exist hook")
+        
+        XCTAssertTrue(postRewriteHookReplaceSuccess!)
+        XCTAssertTrue(postCommitHookReplaceSuccess!)
+        
+        sut.doAction()
+
+        let postRewriteAppendedSuccess = try? sut.postRewriteHookPath.read().contains(sut.postRewriteHook)
+        let postCommitHookAppendedSuccess = try? sut.postCommitHookPath.read().contains(sut.postCommitHook)
+        
+        XCTAssertTrue(postRewriteAppendedSuccess!)
+        XCTAssertTrue(postCommitHookAppendedSuccess!)
+        
+        addTeardownBlock {
+            try? sut.postCommitHookPath.delete()
+            try? sut.postRewriteHookPath.delete()
+        }
     }
     
     override class func tearDown() {
