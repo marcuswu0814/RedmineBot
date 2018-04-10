d6f9119d8558fb3a9bd8cd99ae435a59603d2b91
Marcus Wu <marcus.wu0814@gmail.com>
2018-04-11 01:53:14 +0800

    Fix String+RedmineBot bug
    The pattern is `#` prefix and more than one number sufix

--- Tests/RedmineBotTest/String+RedmineBotTest.swift
+++ Tests/RedmineBotTest/String+RedmineBotTest.swift
@@ -5,17 +5,19 @@ import Foundation
 class StringRedmineBotTest: XCTestCase {
     
     func test__givenString__shuldFindFormatLikeIssueNumber() {
-        let string = "[#33456, #12345, #67890] This is a test, and #13579, #24680"
+        let string = "[#33456, #12345, #67890] This is a test, and #13579, #24680, ##99999"
         
         let matches = string.findIssuesNumber()
         
         XCTAssertNotNil(matches)
-        
+        XCTAssertTrue(matches!.count == 6)
+
         XCTAssertTrue(matches!.contains("#33456"))
         XCTAssertTrue(matches!.contains("#12345"))
         XCTAssertTrue(matches!.contains("#67890"))
         XCTAssertTrue(matches!.contains("#13579"))
         XCTAssertTrue(matches!.contains("#24680"))
+        XCTAssertTrue(matches!.contains("#99999"))
     }
     
     func test__givenStringHadMultiSameIssueNumbers__shouldOnlyFindOne() {
@@ -25,7 +27,7 @@ class StringRedmineBotTest: XCTestCase {
         
         XCTAssertNotNil(matches)
         
-        XCTAssertTrue(matches?.count == 1)
+        XCTAssertTrue(matches!.count == 1)
         XCTAssertTrue(matches!.contains("#12345"))
     }
     
