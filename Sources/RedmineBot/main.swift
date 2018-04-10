8fbdd89395e4e2aacf5e82576af9aecdf4153afb
Marcus Wu <marcus.wu0814@gmail.com>
2018-04-11 01:50:23 +0800

    Remove CommentCommand

--- Sources/RedmineBot/main.swift
+++ Sources/RedmineBot/main.swift
@@ -7,7 +7,6 @@ Group {
     
     $0.addCommand("setup", SetupCommand.make())
     $0.addCommand("install-hook", InstallHookCommand.make())
-    $0.addCommand("comment", CommentCommand.make())
     $0.addCommand("version", VersionCommand.make())
 
     $0.addCommand("post-commit-hook", PostCommitHookCommand.make())
